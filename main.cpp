62c980a4f22fa0a28bac53710c14e1f97e2b59a4
VarunAyyappan <varunayyappan@gmail.com>
2017-02-20 11:12:45 -0800

    Hello world

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Varun on 2/20/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
